d9a1ab5cb709a841ada54f0b049b99df7f41b58e
Loyer Aurélien <aurelien.loyer@zenika.com>
2017-03-02 17:37:19 +0100

    Find over agolia api + display multiple content pending

--- rao-front/src/components/Document.vue
+++ rao-front/src/components/Document.vue
@@ -1,20 +1,39 @@
 <template>
   <div class="document">
-    <strong>Document : {{item.Title}}</strong><br>
-    Client : <strong>{{item.Client}}</strong><br>
-    Région : <strong>{{item.Region}}</strong>
+    <div class="title">
+      <strong>{{item.Title}}</strong>
+    </div>
+    <div class="flex">
+      <div class="informations">
+        Client : <strong>{{item.Client}}</strong><br>
+        Région : <strong>{{item.Region}}</strong><br>
+        Type : <strong>{{item.Mime}}</strong><br>
+      </div>
+      <div class="content">
+        <v-contents :content="item.Content" :search="search"></v-contents>
+      </div>
+    </div>
+    <div class="path">
+      <span><i class="fa fa-folder-open-o" aria-hidden="true"></i> <strong>{{item.Path}}</strong></span>
+    </div>
   </div>
 </template>
 
 <script>
+import Contents from './Contents'
+
 export default {
   name: 'document',
+  components: {
+    'v-contents': Contents
+  },
   data () {
     return {
     }
   },
   props: [
-    'item'
+    'item',
+    'search'
   ],
   created () {
 
@@ -29,7 +48,46 @@ export default {
 .document{
   display: block;
   border: solid 3px $red_znk;
-  padding: 10px;
+  overflow: hidden;
+
+  .title{
+    font-size: 20px;
+    padding: 10px;
+  }
+
+  .flex{
+    display: flex;
+    align-items: center;
+
+    .informations{
+      min-width: 220px;
+      text-align: left;
+      padding-left: 50px;
+    }
+  }
+
+  .path{
+    margin-top: 10px;
+    background: #dfe0dc;
+    padding: 10px;
+    cursor: pointer;
+    transition: all 0.2s;
+
+    span{
+      transition: all 0.2s;
+      display: flex;
+      justify-content: center;
+      i{
+        font-size: 20px;
+        margin-right: 20px;
+      }
+    }
+    &:hover{
+      span{
+        transform: scale(0.9);
+      }
+    }
+  }
 }
 
 </style>
--- rao-front/src/components/Home.vue
+++ rao-front/src/components/Home.vue
@@ -1,27 +1,40 @@
 <template>
   <div class="home largeur">
-    <div class="loading" v-show="1">
-      <img src="https://www.zenika.com/assets/logos/znk.png">
-      <h1>{{ msg }}</h1>
-    </div>
+
+    <h1>
+      G<span>énérateur</span>
+      R<span>éponse</span>
+      A<span>ppel</span>
+      O<span>ffre</span>
+    </h1>
+
+
     <v-search @search="search"></v-search>
     <div class="row">
-      <div class="col-md-2">
-        <v-result></v-result>
+      <div class="col-md-2" v-if="documents.length">
+        <v-result :documents="documents"></v-result>
         <v-filter></v-filter>
       </div>
       <div class="col-md-10">
         <ul class="list_documents">
           <li v-for="doc in documents" >
-            <v-document :item="doc"></v-document>
+            <v-document :item="doc" :search="searching"></v-document>
           </li>
         </ul>
       </div>
+    </div>
+
+    <div class="explain" v-if="documents.length === 0">
+      🦄 🦄 🦄 <br><br>
+      ici un super tuto qui disparait si recherche <br><br>
+      🦄 🦄 🦄
     </div>
   </div>
 </template>
 
 <script>
+/* global algoliasearch */
+/* eslint no-undef: "error" */
 
 import Search from './Search'
 import Filter from './Filter'
@@ -33,7 +46,8 @@ export default {
   data () {
     return {
       documents: [],
-      msg: 'Welcome to RAO a Vue.js App'
+      msg: 'Welcome to RAO a Vue.js App',
+      searching: ''
     }
   },
   components: {
@@ -43,15 +57,28 @@ export default {
     'v-document': Doc
   },
   created () {
-    let url = '/static/data/documents.json'
-
-    this.$http.get(url).then(response => {
-      this.documents = response.body
-    })
   },
   methods: {
     search (value) {
-      console.log(value)
+      let url = '/static/data/documents.json'
+
+      this.$http.get(url).then(response => {
+        this.documents = response.body
+      })
+
+      if (value.length < 400) { // hack disable agolia :)
+        console.log('limit size')
+        return
+      }
+      if (typeof algoliasearch === 'undefined') return
+
+      let client = algoliasearch('0J8NY0SIDS', 'eadffccfb3dd0d42e558717fb423c5a3')
+      let index = client.initIndex('rao')
+
+      index.search(value, (err, content) => {
+        if (content.hits) this.documents = content.hits
+        if (err) console.log(err)
+      })
     }
   }
 }
@@ -60,7 +87,17 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
+h1{
+  margin: 20px auto;
+  span{
+    font-size: 12px;
+  }
+}
 
+.explain{
+  background: #dfe0dc;
+  padding: 20px;
+}
 
 .list_documents{
   padding: 0;
