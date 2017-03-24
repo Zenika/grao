fce85df7a1308003d66934b6aa86f914a97e6a30
qlevaslot <quentin.levaslot@gmail.com>
2017-03-24 11:20:38 +0100

    Update dev settings to match nginx conf

--- rao-front/config/dev.env.js
+++ rao-front/config/dev.env.js
@@ -3,6 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://10.0.10.252:8090/api/v1/search"'
+  API_URL: '"http://127.0.0.1:8090/api/v1/search"'
   // API_URL: '"/static/data/algolia.json"'
 })
