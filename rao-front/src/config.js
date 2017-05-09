0f3e66d4cd54e6cc9a6469e9809876716c21f0f0
Loyer Aurélien <aurelien.loyer@zenika.com>
2017-05-09 17:06:43 +0200

    Add front config file

--- rao-front/src/config.js
+++ rao-front/src/config.js
@@ -1,9 +1,16 @@
 const config = {
-  hidden_facets: [
-    'Path',
-    'Ext',
-    'Title'
-  ]
+  hidden_facets: {
+    rao: [
+      'Path',
+      'Ext',
+      'Title'
+    ],
+    adv: [
+      'Path',
+      'Ext',
+      'Title'
+    ]
+  }
 }
 
 export default config
