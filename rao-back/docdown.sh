411ee88e5893ebc8f6e049c9d765a2a0babe8361
Antoine Cordier <antoine.cordier@zenika.com>
2017-05-10 15:21:35 +0200

    comment docdown script

--- rao-back/docdown.sh
+++ rao-back/docdown.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# This util scripts generates package documentation
+# as github mardown that can be committed to the
+# repository
+# To document a package use godoc comment in source file
+# and add it to the $PACKAGE array below
+# Packages will be appended to a README.md file in
+# same order as they were declared in the $PACKAGE array
 COMMAND="$GOPATH/bin/godocdown"
 DEST="documentation"
 declare -a PACKAGES=(
