ee3a4a51e29a81e37ba03da9b62966fa88df2b56
Travis <travis@travis-ci.org>
2017-09-08 17:38:23 +0200

    Remove login button

--- rao-front/src/components/login/login.vue
+++ rao-front/src/components/login/login.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="col-sm-4 col-sm-offset-4">
-    <h2>G.R.A.O</h2>
-    <h4>by Zenika</h4>
-    <button class="btn btn-primary" @click="login()">Login</button>
   </div>
 </template>
 
